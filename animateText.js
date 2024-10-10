a8caca75b6363358bbe55ec96667004cf87b8fdf
bjeker <rbieke1@gmail.com>
2024-10-10 02:49:26 -0500

    added fade in for text and icons

---
+++ animateText.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.introduction').fadeIn(1000);
+    $('.languages').delay(1 * 250).fadeIn(1000);
+    $('.project-text h1, .projects span, .project-img').fadeIn(1000);
+});
