754aa5e9957d2ce70629f7e51ad0b3dab221ca3a
bjeker <rbieke1@gmail.com>
2024-10-19 03:08:47 -0500

    added fade in for nav bar

--- animateText.js
+++ animateText.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $('.introduction').fadeIn(1000);
+    $('.top-nav').fadeIn(1000);
     $('.languages').delay(1 * 250).fadeIn(1000);
     $('.project-text h1, .projects span, .project-img').fadeIn(1000);
 });
