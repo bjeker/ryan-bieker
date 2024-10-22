ab8bd439807c7fa2d99167d1dd5374e23e60c59a
bjeker <rbieke1@gmail.com>
2024-10-22 16:21:13 -0500

    added fade in for hr

--- animateText.js
+++ animateText.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.introduction').fadeIn(1000);
     $('.top-nav').fadeIn(1000);
+    $('hr').fadeIn(1000);
     $('.project-text').fadeIn(1000);
     $('.languages').delay(1 * 250).fadeIn(1000);
     $('.project-text h1, .projects span, .project-img').fadeIn(1000);
