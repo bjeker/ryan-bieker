d3a007c1e193faea558a74636a4b0d9ca6cdf20b
bjeker <rbieke1@gmail.com>
2026-03-01 15:44:53 -0600

    Updated Books and fixed spacing for headers

--- animateText.js
+++ animateText.js
@@ -3,9 +3,7 @@ $(document).ready(function () {
   $(".top-nav").fadeIn(1000);
   $("hr").fadeIn(1000);
   $(".project-text").fadeIn(1000);
-  $(".languages")
-    .delay(1 * 250)
-    .fadeIn(1000);
+  $(".languages").fadeIn(1000);
   $(".project-text h1, .projects span, .project-img").fadeIn(1000);
 });
 
